93d25f056b247641fd55ed989242a2138447f68b
karan rajak <karanrajak154@gmail.com>
2023-03-07 21:21:53 +0530

    Add Contact Us form

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,48 +1,75 @@
-import React from 'react';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "./Contact.css";
+import {FiSend} from "react-icons/fi";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_5d90iid",
+        "template_be6xmy9",
+        form.current,
+        "VMdu71auH8w4rRCj3",
+        {
+          from_name: "from_name",
+          user_email: "user_email",
+          message: "message",
+        }
+      )
+      .then(
+        (result) => {
+          alert("Thank you for getting in touch!")
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <>
-    <div style={{background:"black",height:"80vh",textAlign:"center",display:"flex",justifyContent:"center"}}>
+    <div className="contact_container">
+    <form ref={form} onSubmit={sendEmail}>
+      <div className="row">
+        <div className="col-25">
+      <label className="label1">Name</label>
+      </div>
+      <div className="col-75">
+      <input className="input_box" type="text" name="from_name" placeholder="Enter Name" />
+      </div>
+      </div>
+      
+      <div className="row">
+        <div className="col-25">
+        <label  className="label1">Email</label>
+        </div> 
 
-<div style={{background:"#251f1f",color:"white",width:"600px",height:"auto",margin:"50px",padding:"35px",borderRadius:"12px"}}>
-    <div style={{background:"red",margin:"10px"}}>   
-    <h3>Get in touch...</h3>
-    <p>Interested to collaborate?</p>
-    <p>Feel Free to drop me an email</p>
+        <div className="col-75">
+        <input className="input_box" type="email" name="user_email" placeholder="Enter Email" />
+        </div>
+      </div>
+      
+      <div className="row">
+        <div className="col-25">
+        <label  className="label1">Message</label>
+        </div> 
+
+        <div className="col-75">
+        <textarea style={{height:"200px"}} className="input_box" name="message" placeholder="Enter Message"/>
+        </div>
+      </div>
+   
+   <div style={{display:"flex",justifyContent:"center",margin:"20px"}}>
+   <input style={{   background:  "aqua",borderRadius:"6px",padding:"12px"}} type="submit" value="Send" /><span><FiSend/></span>
+
+   </div>
+    </form>
     </div>
-
-    <div style={{background:"green"}} >
-        <form>
-            <input
-            // className='input-box '
-            style={{margin:"10px",width:"auto",borderRadius:"8px"}}
-            type="email"
-            placeholder='Your Email ID'
-            />
-            <br/>
-
-            <textarea
-            // className='input-box1 '
-            style={{    margin: "10px",
-              width: "auto",
-              borderRadius: "8px"}}
-            type="text"
-            placeholder='Your Message'
-            />
-<br/>
-            <button
-            // className='submit-btn'
-            >Send Email</button>
-        </form>
-  
-    </div>
-
-    </div>
-    </div>
-    </>
-  )
-}
-
-export default Contact
+  );
+};
+export default Contact;
